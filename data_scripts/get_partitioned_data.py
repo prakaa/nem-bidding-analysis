eb28c8f424d98e618f8568824c18e3cc009dc32b
prakaa <abiprakash007@gmail.com>
2023-07-13 17:06:08 +1000

    data processing pipeline

---
+++ data_scripts/get_partitioned_data.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+from create_parquet_partitions_by_column import chunk_file, get_columns
+
+raw_csvs = sorted(Path.glob(Path("data", "raw"), "*.CSV"))
+if not (output_dir := Path("data", "partitioned")).exists():
+    output_dir.mkdir()
+for csv in raw_csvs:
+    cols = get_columns(csv)
+    if "TRADINGDATE" in cols:
+        partition_col = "TRADINGDATE"
+    elif "SETTLEMENTDATE" in cols:
+        partition_col = "SETTLEMENTDATE"
+    chunk_file(csv, output_dir, partition_col, chunksize=10**6)
