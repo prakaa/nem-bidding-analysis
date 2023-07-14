635cb693c16f28d587bb232eb5d90a527a7d0250
prakaa <abiprakash007@gmail.com>
2023-07-14 18:17:53 +1000

    rebid counts across july

--- analysis_scripts/rebidding_analysis.py
+++ analysis_scripts/rebidding_analysis.py
@@ -1,13 +1,13 @@
 # %%
 from datetime import datetime, timedelta
 from pathlib import Path
+from typing import List
 
 # import matplotlib.pyplot as plt
 import pandas as pd
 import polars as pl
 
 
-# %%
 def get_gen_tech_color_mapping(path_to_mappings: Path) -> pd.DataFrame:
     gen_loads = pd.read_csv(path_to_mappings / Path("cleaned_gen_loads.csv"))
     non_gen_loads = pd.read_csv(
@@ -47,6 +47,7 @@ def get_bid_data_for_periods(
     period_end: int,
 ) -> pd.DataFrame:
     """
+    Day should be a datetime with day, year and month
     NEM day starts at 4AM, hence add 4 hours in addition to PERIODID
     """
     q = pl.scan_parquet(
@@ -78,6 +79,9 @@ def get_all_rebids_less_than_ahead_time(
     """
     There appears to be a large number of bids submitted after the dispatch interval
     of interest. Hence the lower bound of `> pd.Timedelta(minutes=0)`.
+
+    This method will still capture bids that might have been submitted after
+    (informal) gate closure.
     """
     filtered = df[df["REBIDAHEADTIME"] > pd.Timedelta(minutes=0)]
     filtered = filtered[filtered["REBIDAHEADTIME"] <= ahead_time]
@@ -86,7 +90,11 @@ def get_all_rebids_less_than_ahead_time(
 
 def count_rebids_by_tech(
     df: pd.DataFrame, ahead_time: timedelta, path_to_mappings: Path
-):
+) -> pd.DataFrame:
+    """
+    For a set of bids at a particular offer time, we drop duplicates for a DUID
+    to avoid treating energy and FCAS bids submitted at the same time as different bids
+    """
     mapping = get_gen_tech_color_mapping(path_to_mappings)
     filtered = get_all_rebids_less_than_ahead_time(df, ahead_time)
     merged = pd.merge(
@@ -96,24 +104,79 @@ def count_rebids_by_tech(
         how="left",
     )
     merged["Tech"].fillna("Unknown", inplace=True)
-    return merged
+    rebid_cols = [col for col in merged.columns if "TIME" in col] + [
+        "DUID",
+        "Tech",
+    ]
+    rebid = merged[rebid_cols].drop_duplicates()
+    rebid = rebid.groupby("Tech")["DUID"].count().rename("REBIDS")
+    return rebid
 
 
-# x = count_rebids_by_tech(test, timedelta(minutes=10), Path("data", "mappings"))
+def rebid_counts_across_day(
+    partitioned_data_path: Path,
+    path_to_mappings: Path,
+    trading_year: int,
+    trading_month: int,
+    trading_day: int,
+    ahead_time: timedelta,
+) -> pd.DataFrame:
+    trading_date = datetime(trading_year, trading_month, trading_day)
+    if trading_date < datetime(2021, 3, 1):
+        day_col = "SETTLEMENTDATE"
+    else:
+        day_col = "TRADINGDATE"
+    df = get_bid_data_for_periods(
+        partitioned_data_path,
+        day_col,
+        trading_date,
+        1,
+        288,
+    )
+    counts = {}
+    for period_id in range(1, 289):
+        trading_datetime = trading_date + pd.Timedelta(
+            hours=4, minutes=(5 * period_id)
+        )
+        period_df = df[df.PERIODID == period_id]
+        period_counts = count_rebids_by_tech(
+            period_df, ahead_time, path_to_mappings
+        )
+        counts[trading_datetime] = period_counts
+    counts = pd.DataFrame.from_dict(counts, orient="index")
+    return counts
+
+
+def rebid_counts_across_july(
+    years: List[int],
+    ahead_time: timedelta,
+) -> None:
+    month = 7
+    ahead_seconds = int(ahead_time.total_seconds())
+    for year in years:
+        month_data: List[pd.DataFrame] = []
+        for day in range(1, 31):
+            day_count = rebid_counts_across_day(
+                Path("data", "partitioned"),
+                Path("data", "mappings"),
+                year,
+                month,
+                day,
+                ahead_time,
+            )
+            month_data.append(day_count)
+        month_df = pd.concat(month_data, axis=0)
+        month_df.to_parquet(
+            Path(
+                "data",
+                "processed",
+                f"rebid_counts_{month}_{year}_{ahead_seconds}.parquet",
+            )
+        )
+
+
 #
 # # %%
-# rebid_cols = [col for col in df.columns if "TIME" in col] + [
-#     "DUID",
-#     "Technology Type - Descriptor",
-# ]
-# rebid = df[rebid_cols].drop_duplicates()
-# # %%
-# x = (
-#     rebid.groupby("Technology Type - Descriptor")["DUID"]
-#     .count()
-#     .rename("REBIDS")
-# )
-#
 # plt.style.use("../matplotlibrc.mplstyle")
 # plt.pie(x, labels=x.index)
 # plt.legend(loc="lower center")
@@ -131,14 +194,3 @@ def count_rebids_by_tech(
 # plt.style.use("../matplotlibrc.mplstyle")
 # plt.pie(z, labels=z.index)
 # plt.legend(loc="lower center")
-#
-# # %%
-#
-# # %%
-# z = rebid[rebid["Technology Type - Descriptor"] == "Unknown"]
-# z = z.groupby("DUID")["DUID"].count().rename("REBIDS")
-# plt.style.use("../matplotlibrc.mplstyle")
-# plt.pie(z, labels=z.index)
-# plt.legend(loc="lower center")
-# # %%
-#
